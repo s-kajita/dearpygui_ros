8ccd9469a6b1ae7bf0066ed87c91e760e48fb5a1
Shuuji Kajita <shuuji.kajita@gmail.com>
2023-10-09 10:21:06 +0900

    dearpygui_listenerX.py  non-recommended style

---
+++ scripts/dearpygui_listenerX.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+## Simple talker demo that listens to std_msgs/Strings published 
+## to the 'chatter' topic
+
+import os
+import rospy
+from std_msgs.msg import Float32
+import dearpygui.dearpygui as dpg
+
+def subscribe_cb(data):
+    if dpg.get_value("checkbox_sub"):
+        dpg.set_value("slider1", data.data)
+
+
+def main():
+    rospy.init_node('dearpy_listenerX', anonymous=False)
+
+    #-------- DearPy GUI ---------------------
+    dpg.create_context()
+    dpg.create_viewport(title=os.path.basename(__file__), width=810, height=310)
+    dpg.set_global_font_scale(2.5)
+
+    with dpg.window(label="window", width=800, height=300 ,pos=[5,5]):
+        dpg.add_slider_float(tag="slider1",label="SLIDER",default_value=0, max_value=100,min_value=-100)
+        dpg.add_checkbox(label="sub", tag="checkbox_sub", default_value=True)
+        dpg.add_text('',tag="Status")
+
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    
+    rate = rospy.Rate(10) # 10hz
+    #while not rospy.is_shutdown():
+    while dpg.is_dearpygui_running():
+        sub = rospy.Subscriber('slider_value', Float32, subscribe_cb)    # ループ毎にサブスクライブ？
+        dpg.render_dearpygui_frame()
+        rate.sleep()
+        
+    dpg.destroy_context()
+
+if __name__ == '__main__':
+    main()
